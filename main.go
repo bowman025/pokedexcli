b534d46af29fbb043a0089bb568a978e42da8435
bowman025 <rastko.andonov@gmail.com>
2026-09-08 13:52:59 +0200

    Create start repl function and move logic there

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex >")
-		scanner.Scan()
-		input := scanner.Text()
-		clean := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", clean[0])
-		scanner.Err()
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Error reading input: %v\n", err)
+			}
+			break
+		}
+
+		input := scanner.Text()
+		clean := cleanInput(input)
+
+		if len(clean) == 0 {
+			continue
+		}
+
+		command := clean[0]
+		fmt.Printf("Your command was: %s\n", command)
+	}
+
+}
 
 func cleanInput(text string) []string {
 	lowText := strings.ToLower(text)
